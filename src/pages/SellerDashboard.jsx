6a8915c6d1f83888346ebe4efd1a267030d03edc
sujithannabomina <sujith.boss01@gmail.com>
2025-08-03 03:53:29 +0530

    Add files via upload

---
+++ src/pages/SellerDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SellerDashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold">Seller Dashboard</h1>
+    </div>
+  );
+}
