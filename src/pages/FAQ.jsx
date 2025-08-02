8efba61d74d9cdef2928b5955bee6f550dc4b627
sujithannabomina <sujith.boss01@gmail.com>
2025-08-03 03:51:23 +0530

    Delete src directory

--- src/pages/FAQ.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function FAQ() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1 className="text-3xl font-bold">FAQ Page</h1>
-    </div>
-  );
-}
--- src/pages/RefundPolicy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RefundPolicy() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1 className="text-3xl font-bold">Refund Policy Page</h1>
-    </div>
-  );
-}
