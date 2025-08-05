f7daca8784fb7a4b2579311f9edb36b33c6675a1
sujithannabomina <sujith.boss01@gmail.com>
2025-08-05 11:51:12 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import BuyerDashboard from './pages/BuyerDashboard'
-import SellerDashboard from './pages/SellerDashboard'
-import FAQ from './pages/FAQ'
-import Contact from './pages/Contact'
-import RefundPolicy from './pages/RefundPolicy'
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import BuyerDashboard from "./pages/BuyerDashboard";
+import SellerDashboard from "./pages/SellerDashboard";
+import BuyerLogin from "./pages/BuyerLogin";
+import SellerLogin from "./pages/SellerLogin";
+import FAQ from "./pages/FAQ";
+import Contact from "./pages/Contact";
+import RefundPolicy from "./pages/RefundPolicy";
 
 function App() {
   return (
@@ -13,11 +14,13 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/buyer" element={<BuyerDashboard />} />
       <Route path="/seller" element={<SellerDashboard />} />
+      <Route path="/buyer-login" element={<BuyerLogin />} />
+      <Route path="/seller-login" element={<SellerLogin />} />
       <Route path="/faq" element={<FAQ />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/refund" element={<RefundPolicy />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
