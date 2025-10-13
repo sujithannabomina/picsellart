f0077d227dcde485b14ce110bff85df174ffb76d
sujithannabomina <sujith.boss01@gmail.com>
2025-10-13 14:13:18 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,50 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-import Home from './pages/Home'
-import Explore from './pages/Explore'
-import Refund from './pages/Refund'
-import Contact from './pages/Contact'
-import FAQ from './pages/FAQ'
-import SellerLogin from './pages/SellerLogin'
-import BuyerLogin from './pages/BuyerLogin'
-import SellerOnboarding from './pages/SellerOnboarding'
-import SellerStart from './pages/SellerStart'
-import SellerSubscribe from './pages/SellerSubscribe'
-import SellerDashboard from './pages/SellerDashboard'
-import BuyerDashboard from './pages/BuyerDashboard'
-import PhotoDetails from './pages/PhotoDetails'
-import { RequireAuth, RequireSellerActive } from './routes/guards'
+// src/App.jsx
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-export default function App() {
+import LandingPage from './pages/LandingPage';
+import Explore from './pages/Explore';
+import PhotoDetails from './pages/PhotoDetails';
+import Refund from './pages/Refund';
+import Contact from './pages/Contact';
+import Faq from './pages/Faq';
+
+import SellerLogin from './pages/SellerLogin';
+import SellerStart from './pages/SellerStart';
+import SellerOnboarding from './pages/SellerOnboarding';
+import SellerSubscribe from './pages/SellerSubscribe';
+import SellerDashboard from './pages/SellerDashboard';
+
+import BuyerLogin from './pages/BuyerLogin';
+import BuyerDashboard from './pages/BuyerDashboard';
+
+function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/photo/:id" element={<PhotoDetails />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/refund" element={<Refund />} />
-          <Route path="/contact" element={<Contact />} />
+      <Routes>
+        {/* public routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/photo/:id" element={<PhotoDetails />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/refund" element={<Refund />} />
+        <Route path="/contact" element={<Contact />} />
 
-          {/* Buyer */}
-          <Route path="/buyer/login" element={<BuyerLogin />} />
-          <Route
-            path="/buyer/dashboard"
-            element={
-              <RequireAuth to="/buyer/login">
-                <BuyerDashboard />
-              </RequireAuth>
-            }
-          />
+        {/* buyer auth + dashboard */}
+        <Route path="/buyer/login" element={<BuyerLogin />} />
+        <Route path="/buyer/dashboard" element={<BuyerDashboard />} />
 
-          {/* Seller */}
-          <Route path="/seller/login" element={<SellerLogin />} />
-          <Route path="/seller/onboarding" element={<SellerOnboarding />} />
-          <Route
-            path="/seller/start"
-            element={
-              <RequireAuth to="/seller/login">
-                <SellerStart />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/seller/subscribe"
-            element={
-              <RequireAuth to="/seller/login">
-                <SellerSubscribe />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/seller/dashboard"
-            element={
-              <RequireSellerActive>
-                <SellerDashboard />
-              </RequireSellerActive>
-            }
-          />
-        </Routes>
-      </AuthProvider>
+        {/* seller onboarding flow */}
+        <Route path="/seller/login" element={<SellerLogin />} />
+        <Route path="/seller/start" element={<SellerStart />} />
+        <Route path="/seller/onboarding" element={<SellerOnboarding />} />
+        <Route path="/seller/subscribe" element={<SellerSubscribe />} />
+        <Route path="/seller/dashboard" element={<SellerDashboard />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
+
+export default App;
