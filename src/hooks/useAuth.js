1eddd311a90d2aa1325f0bed65419f47d100865e
sujithannabomina <sujith.boss01@gmail.com>
2026-02-15 23:40:05 +0530

    Delete src directory

--- src/hooks/useAuth.js
+++
@@ -1,2 +0,0 @@
-// src/hooks/useAuth.js
-export * from "./useAuth.jsx";
--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-// File: src/pages/Home.jsx
-
-import React from "react";
-import LandingPage from "./LandingPage";
-
-export default function Home() {
-  return <LandingPage />;
-}
